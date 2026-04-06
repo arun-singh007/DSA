aa7e44a5daa93efd01ee3fe5c4b25fd2b898e1a6
arun-singh007 <krarun10199@gmail.com>
2026-04-06 22:10:42 +0530

    Time: 629 ms (60.64%) | Memory: 22.2 MB (86.32%) - LeetSync

--- Two-Pointer/15-3sum/3sum.py
+++ Two-Pointer/15-3sum/3sum.py
@@ -2,20 +2,20 @@ class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
         nums.sort()
         res = []
-        for i in range(len(nums) - 2):
+        for i in range(len(nums)-2):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
             left = i+1
             right = len(nums) - 1
             while left < right:
-                sumthree = nums[i] + nums[left] + nums[right]
-                if sumthree < 0:
+                add = nums[i] + nums[left] + nums[right]
+                if add < 0:
                     left += 1
-                elif sumthree > 0:
+                elif add > 0:
                     right -= 1
                 else:
-                    res.append([nums[i] , nums[left] , nums[right]])
-                    while left < right and nums[left] == nums[left + 1]:
+                    res.append([nums[i],nums[left],nums[right]])
+                    while left < right and nums[left] == nums[left +1 ]:
                         left += 1
                     while left < right and nums[right] == nums[right - 1]:
                         right -= 1
@@ -23,7 +23,4 @@ class Solution:
                     right -= 1
         return res
 
-
-
-        
         
