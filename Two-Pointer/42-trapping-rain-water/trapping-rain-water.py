ccc052fd3a6e0dcf603b98948af42b8a2c950bc8
arun-singh007 <krarun10199@gmail.com>
2026-04-18 17:10:05 +0530

    Time: 8 ms (56.27%) | Memory: 21 MB (56.00%) - LeetSync

---
+++ Two-Pointer/42-trapping-rain-water/trapping-rain-water.py
@@ -0,0 +1,18 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        left_max = height[left]
+        right_max = height[right]
+        result = 0
+        while left < right:
+            if left_max < right_max:
+                result += min(left_max,right_max) - height[left]
+                left += 1
+                left_max = max(left_max,height[left])
+            else:
+                result += min(left_max,right_max) - height[right]
+                right -= 1
+                right_max = max(right_max,height[right])
+        return result
+        
